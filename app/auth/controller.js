9e70d06ce6dd737a00d3d5d1135a6c52c38f82da
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-04-30 22:58:23 +0700

    integrating zk

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,7 +1,20 @@
+const { buildPoseidon } = require('circomlibjs');
+const path = require('path');
+const fs = require('fs');
+
+const vKey = JSON.parse(fs.readFileSync(path.join(__dirname, "../../zk/verification-key.json")))
+
+let address = []
+
 module.exports = {
     register: async(req, res) => {
         try {
-            
+            const { address } = req.body
+
+            if(!address) {
+                return res.status(400).json({ message: 'Address is required' })
+            }
+
         } catch (error) {
             res.status(500).json({ message: 'Internal Server Error' });
         }
