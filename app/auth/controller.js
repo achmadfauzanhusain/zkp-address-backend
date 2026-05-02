2965ed15d09b364dddd234e9682fae64d8233332
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-02 20:34:58 +0700

    feat(auth): login

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,10 +1,15 @@
 const { buildPoseidon } = require('circomlibjs');
+const snarkjs = require('snarkjs');
 const path = require('path');
 const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const { hash } = require('crypto');
 
 const vKey = JSON.parse(fs.readFileSync(path.join(__dirname, "../../zk/verification-key.json")))
 
-let addresses = ["1246254232611659900859275708950704372506712141612608580836537858378470542983"]
+let addresses = {
+    "1246254232611659900859275708950704372506712141612608580836537858378470542983": "1246254232611659900859275708950704372506712141612608580836537858378470542983"
+}
 
 module.exports = {
     register: async(req, res) => {
@@ -27,29 +32,29 @@ module.exports = {
         try {
             const { proof, publicSignals } = req.body
 
-            // const checkAddress = addresses[address]
-            // console.log(checkAddress)
+            if(!proof || !publicSignals) {
+                return res.status(400).json({ message: 'you must generate a proof first' })
+            }
 
             const checkAddress = addresses[publicSignals[0]]
-            console.log(checkAddress)
+            if(!checkAddress) {
+                return res.status(400).json({ message: 'Address is not registered' })
+            } else {
+                const verified = await snarkjs.groth16.verify(
+                    vKey,
+                    publicSignals,
+                    proof
+                )
+                if(verified) {
+                    const token = jwt.sign({
+                        hash: publicSignals[0]
+                    }, "test") // secret key
 
-            // if(address.includes(address)) {
-            //     // Address is registered
-            //     const poseidon = await buildPoseidon();
-            //     const hashValue = poseidon([BigInt(address)]);
-            //     const hash = poseidon.F.toString(hashValue);
-
-            //     // Verify the proof using the verification key
-            //     const isValidProof = await snarkjs.groth16.verify(vKey, publicSignals, proof);
-
-            //     if (isValidProof) {
-            //         res.status(200).json({ message: 'Login successful' });
-            //     } else {
-            //         res.status(400).json({ message: 'Invalid proof' });
-            //     }
-            // } else {
-            //     return res.status(400).json({ message: 'Address is not registered' });
-            // }
+                    res.status(201).json({ message: 'Login successful', data: token })
+                } else {
+                    res.status(400).json({ message: 'Invalid proof' })
+                }
+            }
         } catch (error) {
             res.status(500).json({ message: 'Internal Server Error' });
         }
